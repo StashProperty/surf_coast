c25ba74b5162738b8f7d6d9c29f426e1bf1698e3
AndreM101 <56064706+AndreM101@users.noreply.github.com>
2021-03-14 11:55:02 +0800

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,7 @@ require 'scraperwiki'
 require 'rubygems'
 require 'faraday'
 require 'date'
+require 'json'
 
 
 start_date = (Date.today - 14).strftime("%d-%m-%Y")
@@ -17,7 +18,7 @@ resp = Faraday.post(data_url) do |req|
     req.body = '{"packet":[{"name":"iDisplayLength","value":1000},{"name":"bSearchable_0","value":true},{"name":"bSortable_0","value":true},{"name": "sSearch_1", "value": '+ start_date+ '},{"name":"mDataProp_1","value":"LodgedDate_STRING"},{"name":"bRegex_1","value":false},{"name":"bSearchable_1","value":true},{"name":"bSortable_1","value":true},{"name":"mDataProp_2","value":"DecisionDate_STRING"},{"name":"bRegex_2","value":false},{"name":"bSearchable_2","value":true},{"name":"bSortable_2","value":true},{"name":"mDataProp_3","value":"SiteAddress"},{"name":"bRegex_3","value":false},{"name":"bSearchable_3","value":true},{"name":"bSortable_3","value":false},{"name":"mDataProp_4","value":"ReasonForPermit"},{"name":"bRegex_4","value":false},{"name":"bSearchable_4","value":true},{"name":"bSortable_4","value":true},{"name":"mDataProp_5","value":"Ward"},{"name":"sSearch_5","value":""},{"name":"bRegex_5","value":false},{"name":"bSearchable_5","value":true},{"name":"bSortable_5","value":true},{"name":"mDataProp_6","value":"StatusName"},{"name":"bRegex_6","value":false},{"name":"bSearchable_6","value":true},{"name":"bSortable_6","value":false},{"name":"mDataProp_7","value":"Actions"},{"name":"bRegex_7","value":false},{"name":"bSearchable_7","value":true},{"name":"bSortable_7","value":true},{"name":"bRegex","value":false},{"name":"iSortCol_0","value":1},{"name":"sSortDir_0","value":"desc"},{"name":"iSortingCols","value":1}]}'
   end
   
-raw = JSON.parse(resp.body)['d'].split("]")[0].delete("}").delete('"').delete("\\").gsub("null","nil").split("{")[2..-1]
+raw = json.parse(resp.body)['d'].split("]")[0].delete("}").delete('"').delete("\\").gsub("null","nil").split("{")[2..-1]
 
 puts "Found #{raw.length} rows"
 
